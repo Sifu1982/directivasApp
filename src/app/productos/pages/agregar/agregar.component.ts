2c3335dc57f7e47bfc6990a8f1d7dcd6659acc0b
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-25 08:38:15 +0200

    feat: reactive form created

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class AgregarComponent implements OnInit {
+export class AgregarComponent {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  tieneError(campo: string): boolean {
+    return this.miFormulario.get(campo)?.invalid || false;
   }
-
 }
